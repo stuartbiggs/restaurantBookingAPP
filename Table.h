0bb59917f0015c1edf9fca3c29f5aac1d9798a5e
mintyme777 <smbiduk@yahoo.co.uk>
2017-12-21 08:52:16 +0000

    first commit of restaurantbookingapp

--- Table.h
+++ Table.h
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
  * File:   Table.h
@@ -20,6 +15,13 @@ using namespace std;
 class Table
 {
 public:
+    
+    Table()
+    {
+        int TableNum = 0;
+        string TableState = "empty";
+    }
+    
     //function that sets the table number
     void setTableNumber( int tableNumber ) 
     {

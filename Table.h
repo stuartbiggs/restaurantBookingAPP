e146e1eafd5f3e4b06f5c037c8a208b75657f21e
mintyme777 <smbiduk@yahoo.co.uk>
2017-12-21 02:13:30 +0000

    first commit of restaurantbookingapp

---
+++ Table.h
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Table.h
+ * Author: stuartbiggs
+ *
+ * Created on 21 December 2017, 02:03
+ */
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Table
+{
+public:
+    //function that sets the table number
+    void setTableNumber( int tableNumber ) 
+    {
+        tableNumber = TableNum;
+    } //end function setTableNumer
+    
+    //function that gets the table number
+    int getTableNumber() const
+    {
+        return TableNum;
+    } //end function getTableNumber
+    
+    //function that sets the Table state
+    void setTableState( string tableState )
+    {
+        tableState = TableState;
+    } //end function setTableState
+     
+    //function that gets the Table state
+    string getTableState() const
+    {
+        return TableState;
+    } // end function getTableState
+    
+  
+    //function that displays a welcome message
+    void displayMessage() const
+    {
+        cout << "This is the table Application " << endl; // table message
+        cout << "\nThe table number is: " << getTableNumber()
+                << endl;
+    } //end function displayMessage
+    
+    //function that displays the table state
+    void displayTableState() const
+    {
+        cout << "The table status is: " << getTableState() 
+                << endl;
+    } //end function displayTableState
+    
+   
+    
+private:
+    int TableNum = 1;
+    string TableState = "empty";
+};
--- main.cpp
+++ main.cpp
@@ -5,63 +5,9 @@
  * Created on 19 December 2017, 13:20
  */
 
-#include <iostream>
-#include <string>
-using namespace std;
+#include "Table.h" 
 
-class Table
-{
-public:
-    //function that sets the table number
-    void setTableNumber( int tableNumber ) 
-    {
-        tableNumber = TableNum;
-    } //end function setTableNumer
-    
-    //function that gets the table number
-    int getTableNumber() const
-    {
-        return TableNum;
-    } //end function getTableNumber
-    
-    //function that sets the Table state
-    void setTableState( string tableState )
-    {
-        tableState = TableState;
-    } //end function setTableState
-     
-    //function that gets the Table state
-    string getTableState() const
-    {
-        return TableState;
-    } // end function getTableState
-    
-  
-    //function that displays a welcome message
-    void displayMessage() const
-    {
-        cout << "This is the table Application " << endl; // table message
-        cout << "\nThe table number is: " << getTableNumber()
-                << endl;
-    } //end function displayMessage
-    
-    //function that displays the table state
-    void displayTableState() const
-    {
-        cout << "The table status is: " << getTableState() 
-                << endl;
-    } //end function displayTableState
-    
-   
-    
-private:
-    int TableNum = 1;
-    string TableState = "empty";
-};
 
-/*
- * 
- */
 int main() {
 
     Table myTable; // table instance
